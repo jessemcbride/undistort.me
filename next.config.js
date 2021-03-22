3b78a4a6b2888756174ae81de589eb6a0e48e134
Jesse McBride <jm@ucf.edu>
2021-03-22 19:56:25 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   publicRuntimeConfig: {
     siteMetaData: {
-      name: "undistort.me"
-      url:
-        process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/",
+      name: "undistort.me",
+      url: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/",
       title: "undistort.me",
       description: "The cognitive behavioral therapy toolkit",
       twitterHandle: "undistortme",
