22653b5c8dde008ad7183621db87c303eee26082
Jesse McBride <jm@ucf.edu>
2021-03-31 19:34:24 -0400

    fix: add bottom demo link

--- pages/index.tsx
+++ pages/index.tsx
@@ -144,24 +144,25 @@ const Home = ({ data, setData }) => {
             </form>
           </div>
           <div className="flex items-center justify-end px-2">
-            <a href="#" className="text-lg font-medium text-gray-800">
-              <div className="flex items-center space-x-1">
-                <span>Try the demo now</span>
-
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  className="h-5 text-gray-500"
-                  fill="currentColor"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </div>
-            </a>
+            <Link href="/exercises/MoodLog">
+              <a className="text-lg font-medium text-gray-800">
+                <div className="flex items-center space-x-1">
+                  <span>Try the demo now</span>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    className="h-5 text-gray-500"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </a>
+            </Link>
           </div>
         </main>
       </div>
