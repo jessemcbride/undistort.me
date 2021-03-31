115120e8c47c8a620d5fc5268dd1f97f492c74f3
Jesse McBride <jm@ucf.edu>
2021-03-31 15:09:27 -0400

    fix: Update import path

--- pages/exercises/ThoughtRecord/catch-thoughts.tsx
+++ pages/exercises/ThoughtRecord/catch-thoughts.tsx
@@ -1,4 +1,4 @@
-import Layout from "../components/layout";
+import Layout from "../../../components/layout";
 import Link from "next/link";
 
 import SelectionHighlighter from "react-highlight-selection";
