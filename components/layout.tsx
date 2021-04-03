6a259c8ffda35bebac44c9641275c4d0b436f25c
Jesse McBride <jm@ucf.edu>
2021-04-03 12:12:46 -0400

    fix: Add layout meta tag

--- components/layout.tsx
+++ components/layout.tsx
@@ -67,6 +67,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           content={`630`}
           key="og_image_height"
         />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
